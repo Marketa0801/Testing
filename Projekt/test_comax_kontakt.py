2d6d58ecd484450a7240658c2f4363d2a17722d2
Marketa0801 <pudilova.marketa@seznam.cz>
2025-01-02 17:48:56 +0100

    Doplnění odkliknutí cookies

--- Projekt/test_comax_kontakt.py
+++ Projekt/test_comax_kontakt.py
@@ -1,11 +1,31 @@
 #------------------TEST ODKAZU---------------
 #Testuje, zda tlačítko Kontakty z hlavního menu na domovské stránce správně přesměruje.
 import pytest
+from playwright.sync_api import sync_playwright
+
+@pytest.fixture()
+def browser():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(
+            headless=False, slow_mo=1000
+        )
+        yield browser
+        browser.close()
+
+@pytest.fixture()
+def page(browser):
+    page = browser.new_page()
+    yield page
+    page.close()
 
 def test_kontakty(page):
-#Vstup na stránku
-    page.goto("https://www.strechycomax.cz/")  
-#Cookies není třeba odklikávat jelikož je web řeší speciálním odkazem v zápatí stránky. 
+
+
+    #Vstup na stránku
+    page.goto("https://www.strechycomax.cz/") 
+    #Cookies
+    refuse_button = page.locator("div.ch2-dialog-actions button.ch2-deny-all-btn", has_text="Jen nezbytné")
+    refuse_button.click()
 
 # Čekám na prvek, aby byl viditelný a interaktivní
     contact_link = page.locator("#block-comax-main-menu .menu-item a[href='https://www.strechycomax.cz/kontakt']") # Bylo nutné velmi specifikovat lokátor, jelikož na stránce se text="Kontakt" jich nachází mnoho. 
